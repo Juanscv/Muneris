65a52332c470e7fd52cc7b74b68db501693f3d02
juanscv <juanscv@outlook.com>
2014-03-17 10:58:00 -0500

    Images not loading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muneris::Application.config.secret_token = '29573c3fd91f79768d191c1cf0261273ba4a3fb0ebc5c47b8c11c3254916efeca617fb17c7c35b4ad3fab92efb25bd5dcbb4a3ced66948679ccd255d342064f5'
