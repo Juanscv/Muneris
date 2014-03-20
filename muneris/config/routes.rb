d770eab5672ac0361c896a9fafe018e2364ab894
Michael Dussan <michael@uololou.com.br>
2014-03-19 21:44:59 -0300

    routes menu and after session redirect

--- muneris/config/routes.rb
+++ muneris/config/routes.rb
@@ -1,5 +1,12 @@
 Muneris::Application.routes.draw do
 
+  get "/user/:id/feed", to: 'user#feed', as: :feed
+  get "/user/:id/notifications", to: 'user#notifications', as: :notifications
+  get "/user/:id/friends", to: 'user#friends', as: :friends
+  get "/dashboard", to: 'muneris#dashboard', as: :dashboard
+  get "/profile", to: 'muneris#profile', as: :profile
+  get "/network", to: 'muneris#network', as: :network
+  get "/map", to: 'muneris#map', as: :map
   devise_for :users
 
   root to: "welcome#index"
---
+++ muneris/test/helpers/muneris_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MunerisHelperTest < ActionView::TestCase
+end
