f55aebf0e2c3a0bf59ee7742e116c6b44c08cf24
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2014-04-11 00:11:24 -0500

    Loading test data

--- muneris/db/seeds.rb
+++ muneris/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'    
+
+csv_users = File.read('db/data/Clientes.csv')
+data_users = CSV.parse(csv_users, :headers => false)
+csv_locales = File.read('db/data/Locales.csv')
+data_locales = CSV.parse(csv_locales, :headers => false)
+data_users.each do |row|
+	users = User.new(row.to_hash)
+	data_locales.each do |row|
+		if users.locale == row[0]
+			users.locale = [row[3],row[4],row[5]].join(', ')
+		end
+	end
+end
