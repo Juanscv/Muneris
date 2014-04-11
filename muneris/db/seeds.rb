45be329ad9abd0d6c35db3e372760b4599dc9212
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2014-04-11 01:24:09 -0500

    Good seeding

--- muneris/db/seeds.rb
+++ muneris/db/seeds.rb
@@ -8,15 +8,21 @@
 
 require 'csv'    
 
-csv_users = File.read('db/data/Clientes.csv')
+csv_users = File.read('db/data/Clientes-o.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
 data_users = CSV.parse(csv_users, :headers => false)
-csv_locales = File.read('db/data/Locales.csv')
+csv_locales = File.read('db/data/Localidades.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
 data_locales = CSV.parse(csv_locales, :headers => false)
+i=0
 data_users.each do |row|
-	users = User.new(row.to_hash)
-	data_locales.each do |row|
-		if users.locale == row[0]
-			users.locale = [row[3],row[4],row[5]].join(', ')
+	user = User.new
+	user.email=["user",i.to_s,"@gmail.com"].join
+	user.address=row[0]
+	user.tariff=row[1]
+	data_locales.each do |row2|
+		if row[2] == row2[0]
+			user.locale = [row2[2].capitalize,row2[3].capitalize,"Colombia"].join(', ')
 		end
 	end
+	i += 1
+	user.save!(:validate => false)
 end
