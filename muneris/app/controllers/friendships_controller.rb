b94c85c2e5f334cf0882d38ad887511db9ad270c
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2014-05-16 22:21:44 -0500

    Grid improvements

--- muneris/app/controllers/friendships_controller.rb
+++ muneris/app/controllers/friendships_controller.rb
@@ -3,33 +3,43 @@ class FriendshipsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @friends_grid = initialize_grid(
-     User.unscoped.joins('INNER JOIN friendships ON (friendships.friend_id = users.id OR friendships.friendable_id = users.id)').where('users.id not IN (?) AND (friendships.friendable_id= ? OR friendships.friend_id = ?) AND friendships.pending = 0 AND friendships.blocker_id IS NULL', current_user.id, current_user.id, current_user.id),
-     order: 'users.id',
-     per_page: 8
-     )
-
     if params[:user_id].nil? then
       @user = current_user
     else
       @user = User.find(params[:user_id])
     end
 
+    @friends_grid = initialize_grid(
+      User.unscoped.joins('INNER JOIN friendships ON (friendships.friend_id = users.id OR friendships.friendable_id = users.id)').where('users.id not IN (?) AND (friendships.friendable_id= ? OR friendships.friend_id = ?) AND friendships.pending = 0 AND friendships.blocker_id IS NULL', current_user.id, current_user.id, current_user.id),
+      order: 'users.id',
+      with_resultset: :process_records,
+      per_page: 8,
+      name: 'g'
+     )
+
+    @results = []
+
+    if params[:g] && params[:g][:selected]
+      @selected = params[:g][:selected]
+    end
+
   end
 
   def new
+    if params[:user_id].nil? then
+      @user = current_user
+    else
+      @user = User.find(params[:user_id])
+    end
+
     @users_grid = initialize_grid(
       User,
       conditions: ["id != ?", current_user.id],
       order: 'users.id',
       per_page: 8
     )
-    if params[:user_id].nil? then
-      @user = current_user
-    else
-      @user = User.find(params[:user_id])
-    end
-end
+    
+  end
 
   def create
     invitee = User.find_by_id(params[:user_id])
@@ -89,6 +99,10 @@ end
     else
       @user = User.find(params[:user_id])
     end
+  end
+
+  def process_records(records)
+    @results = records.find(:all)
   end
   
 end
