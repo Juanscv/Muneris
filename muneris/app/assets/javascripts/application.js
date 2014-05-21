ec0ad4e371ff75a005f9f691f5628ddf5bc5849f
Michael Dussan <michael@uololou.com.br>
2014-05-21 13:23:54 -0300

    working in select

--- muneris/app/assets/javascripts/application.js
+++ muneris/app/assets/javascripts/application.js
@@ -39,3 +39,15 @@ $(function () {
     return false;
   });
 });
+
+
+$(function(){
+    // For each radio button whose name is 'content[id]':
+    jQuery("input[type='radio'][name='content[id]']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).click(function(){
+            // Set the value of 'title' textfield to the radio's title
+            jQuery("#title").val(this.title);
+        });
+    });
+});
