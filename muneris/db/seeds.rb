1364301fe1546dd057e448e646902aa62c50e382
Michael Dussan <michael@uololou.com.br>
2014-05-21 04:21:06 -0300

    working on seed

--- muneris/db/seeds.rb
+++ muneris/db/seeds.rb
@@ -12,17 +12,55 @@ User.create!({:email => "eadmin@gmail.com", :admin => 1, :password => "12345678"
 User.create!({:email => "wadmin@gmail.com", :admin => 2, :password => "12345678", :password_confirmation => "12345678", :address => "Calle 93 # 46", :name => "W_Admin", :locale => "Barranquilla, Atlantico, Colombia"})
 User.create!({:email => "gadmin@gmail.com", :admin => 3, :password => "12345678", :password_confirmation => "12345678", :address => "Calle 93 # 46", :name => "G_Admin", :locale => "Barranquilla, Atlantico, Colombia"})
 
-csv_users = File.read('db/data/Clientes.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
-data_users = CSV.parse(csv_users, :headers => false)
+
 csv_locales = File.read('db/data/Localidades.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
 data_locales = CSV.parse(csv_locales, :headers => false)
-csv_bills = File.read('db/data/Consumos.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
-data_bills= CSV.parse(csv_bills, :headers => true)
-data_bills.to_a!
+
+csv_users1 = File.read('db/data/Clientes_part1.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users1 = CSV.parse(csv_users, :headers => false)
+csv_bills1 = File.read('db/data/Consumos_part1.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills1= CSV.parse(csv_bills, :headers => true)
+data_bills1.to_a!
+
+csv_users2 = File.read('db/data/Clientes_part2.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users2 = CSV.parse(csv_users, :headers => false)
+csv_bills2 = File.read('db/data/Consumos_part2.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills2= CSV.parse(csv_bills, :headers => true)
+data_bills2.to_a!
+
+csv_users3 = File.read('db/data/Clientes_part3.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users3 = CSV.parse(csv_users, :headers => false)
+csv_bills3 = File.read('db/data/Consumos_part3.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills3= CSV.parse(csv_bills, :headers => true)
+data_bills3.to_a!
+
+csv_users4 = File.read('db/data/Clientes_part4.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users4 = CSV.parse(csv_users, :headers => false)
+csv_bills4 = File.read('db/data/Consumos_part4.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills4= CSV.parse(csv_bills, :headers => true)
+data_bills4.to_a!
+
+csv_users5 = File.read('db/data/Clientes_part5.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users5 = CSV.parse(csv_users, :headers => false)
+csv_bills5 = File.read('db/data/Consumos_part5.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills5= CSV.parse(csv_bills, :headers => true)
+data_bills5.to_a!
+
+csv_users6 = File.read('db/data/Clientes_part6.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users6 = CSV.parse(csv_users, :headers => false)
+csv_bills6 = File.read('db/data/Consumos_part6.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills6= CSV.parse(csv_bills, :headers => true)
+data_bills6.to_a!
+
+csv_users7 = File.read('db/data/Clientes_part7.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_users7 = CSV.parse(csv_users, :headers => false)
+csv_bills7 = File.read('db/data/Consumos_part7.csv').force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+data_bills7= CSV.parse(csv_bills, :headers => true)
+data_bills7.to_a!
 
 i = 0
 
-data_users.each do |user|
+data_users7.each do |user|
 	data_locales.each do |locale|
 		if user[2] == locale[0]
 			
@@ -30,8 +68,8 @@ data_users.each do |user|
 				:email => [i.to_s,Faker::Internet.email].join,
 				:password => "12345678",
 				:password_confirmation => "12345678",
-				:tariff => user[1],
-				:address => user[0],
+				:tariff => user[0],
+				:address => user[1],
 				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
 				:familyname => ["User",i.to_s].join
 				})
@@ -41,9 +79,9 @@ data_users.each do |user|
 			1.upto(44) do |j|
 
 				new_bill = Bill.create({   
-					:consumption => data_bills[i][j],
-					:value => data_bills[i][j].to_f*Random.rand(200 .. 500),
-					:date => Date.parse(data_bills[0][j]),
+					:consumption => data_bills7[i][j],
+					:value => data_bills7[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills7[0][j]),
 					:service => 1
 					})
 
@@ -53,3 +91,188 @@ data_users.each do |user|
 	end
 end
 
+data_users6.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills6[i][j],
+					:value => data_bills6[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills6[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
+
+data_users5.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills5[i][j],
+					:value => data_bills5[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills5[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
+
+data_users4.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills4[i][j],
+					:value => data_bills4[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills4[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
+
+data_users3.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills3[i][j],
+					:value => data_bills3[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills3[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
+
+data_users2.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills2[i][j],
+					:value => data_bills2[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills2[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
+
+data_users1.each do |user|
+	data_locales.each do |locale|
+		if user[2] == locale[0]
+			
+			new_user = User.create({
+				:email => [i.to_s,Faker::Internet.email].join,
+				:password => "12345678",
+				:password_confirmation => "12345678",
+				:tariff => user[0],
+				:address => user[1],
+				:locale => [locale[2].capitalize,locale[3].capitalize,"Colombia"].join(', '),
+				:familyname => ["User",i.to_s].join
+				})
+
+			i += 1
+
+			1.upto(44) do |j|
+
+				new_bill = Bill.create({   
+					:consumption => data_bills1[i][j],
+					:value => data_bills1[i][j].to_f*Random.rand(200 .. 500),
+					:date => Date.parse(data_bills1[0][j]),
+					:service => 1
+					})
+
+				new_user.userbills.create!(bill_id: new_bill.id)
+			end
+		end
+	end
+end
