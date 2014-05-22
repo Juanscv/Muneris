fc0e42d6952affecca01986c476158b4d3be480f
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2014-05-21 20:05:38 -0500

    Added Dalli

--- muneris/config/environments/development.rb
+++ muneris/config/environments/development.rb
@@ -11,7 +11,7 @@ Muneris::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -26,6 +26,9 @@ Muneris::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Use Dalli/Memcached
+  config.cache_store = :dalli_store
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
